5aa1a6697a2b0c02fb072d844fc0e83bf9b42977
Muhammet Furkan Yiğit <muhammetfurkanygt@gmail.com>
2025-11-08 12:33:16 +0300

    Initial commit: all microservices and shared-libs

---
+++ shared-libs/src/test/java/com/furkan/sharedlibs/SharedLibsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furkan.sharedlibs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedLibsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
